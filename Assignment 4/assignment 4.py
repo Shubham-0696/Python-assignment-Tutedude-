c29a37d03e1b5ffcb7978acd0e8b2717085ba73d
Shubham-0696 <wise.shubham@gmail.com>
2025-04-25 01:07:23 +0530

    Update assignment 4.py

--- Assignment 4/assignment 4.py
+++ Assignment 4/assignment 4.py
@@ -7,10 +7,10 @@
 
 
 try:
-    with open("sample.txt", 'r') as file:
-        for num, line in enumerate(file, 1):
-            line = line.strip()
-            print(f"line {num}: {line}")
+    file = open("sample.txt", 'r')
+    for num,line in enumerate(file,1):
+        line = line.strip()
+        print(f"line {num} : {line}")
 
 except FileNotFoundError:
     print("ERROR:The file " 'sample.txt' " not found. ")
@@ -22,18 +22,18 @@ except FileNotFoundError:
 
 
 
-file_name="sample.txt"
-with open("sample.txt", "w" ) as file:
+file_name="output.txt"
+with open("output.txt", "w" ) as file:
     first_entry=input("Enter text to write to the file: ")
     file.write(first_entry +"\n")
     print("Data successfully written to " ,file_name, ".")
 
-with open("sample.txt", "a") as file:
+with open("output.txt", "a") as file:
     next_line=input("Enter additional text to append:")
     file.write(next_line)
     print("Data successfully appended.")
 
-with open("sample.txt" , "r") as file  :
+with open("output.txt" , "r") as file  :
     print("Final content of ", file_name,":")
     r = file.read()
     print(r)
